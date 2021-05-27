a1203defadd184ea6ce073e55d862cccf30a039f
Henrique Machado <henriquemachado1993@gmail.com>
2021-05-27 19:10:44 -0300

    [ADD] added the project codes

---
+++ ControleMatricula/FileUtil.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ControleMatricula
+{
+    public static class FileUtil
+    {
+        public static void Save(string fileName, string path, List<string> lstContent)
+        {
+            try
+            {
+                var fullPath = $@"{path}\{fileName}";                
+                using (StreamWriter sw = new StreamWriter(fullPath))
+                {
+                    for (int i = 0; i < lstContent.Count; i++)
+                    {
+                        sw.WriteLine(lstContent[i]);
+                    }               
+                }                
+            }
+            catch (Exception ex)
+            {
+                throw new MissingFieldException(ex.ToString());
+            }
+        }
+
+        public static List<string> Read(string fileName, string path)
+        {
+            try
+            {
+                var fullPath = $@"{path}\{fileName}";                
+                using (StreamReader sr = File.OpenText(fullPath))
+                {
+                    List<string> lst = new List<string>();
+                    string s = string.Empty;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        lst.Add(s);
+                    }
+                    return lst;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new MissingFieldException(ex.ToString());
+            }
+        }
+
+        public static void Delete(string fileName, string path)
+        {
+            try
+            {
+                var fullPath = $@"{path}\{fileName}";                
+                File.Delete(fullPath);
+            }
+            catch (Exception ex)
+            {
+                throw new MissingFieldException(ex.ToString());
+            }   
+        }
+    }
+}
---
+++ Test/Tests/ProgramTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using ControleMatricula;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void ProcessaListaMatriculaSemDv_retorno_lista_de_matriculas()
+        {
+            List<string> lstMatriculas = new List<string>() { "9876", "9992" };
+            var lstMtrComDv = Program.ProcessaListaMatriculaSemDv(lstMatriculas);
+
+            Assert.IsNotNull(lstMtrComDv);
+        }
+
+        [TestMethod]
+        public void ProcessaListaMatriculaSemDv_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                List<string> lstMatriculas = new List<string>() { };
+                Program.ProcessaListaMatriculaSemDv(lstMatriculas);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex; 
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void ProcessaDv_retorno_digito_verificador()
+        {
+            var dvProcessado = "E";
+            var dv = Program.ProcessaDv("9876");
+
+            Assert.AreEqual(dv, dvProcessado);
+        }
+
+        [TestMethod]
+        public void ProcessaDv_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                Program.ProcessaDv("9876e32f");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void VerificarMatriculas_retorno_lista_de_matriculas_verificadas()
+        {
+            List<string> lstMatriculas = new List<string>() { "9876-E", "9992-0", "9992584-E" };
+            var lstMtrVerficadas = Program.VerificarMatriculas(lstMatriculas);
+
+            Assert.IsNotNull(lstMtrVerficadas);
+        }
+
+        [TestMethod]
+        public void VerificarMatriculas_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                Program.VerificarMatriculas(new List<string>() { });
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void LerMatriculas_retorna_lista_de_matriculas()
+        {
+            var lstMatricula = Program.LerMatriculas("matriculasSemDV.txt");
+
+            Assert.IsNotNull(lstMatricula);
+        }
+
+        [TestMethod]
+        public void LerMatriculas_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                Program.LerMatriculas("matriculasSemDVqweqweqweqweqwe.txt");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void GravarMatriculas()
+        {
+            List<string> lstMatriculas = new List<string>() { "9876", "9992" };
+            Program.GravarMatriculas(lstMatriculas, "matriculasSemDV.txt");
+        }
+
+        [TestMethod]
+        public void GravarMatriculas_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                Program.GravarMatriculas(new List<string>() {}, "matriculasSemDV.txt");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void FileUtil_delete()
+        {
+            FileUtil.Delete("matriculasSemDV.txt", @"c:\temp");
+        }
+
+        [TestMethod]
+        public void FileUtil_delete_exception()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                FileUtil.Delete("matriculasSemDV.txt", @"c:\temp342fdsfsdf");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+    }
+}
